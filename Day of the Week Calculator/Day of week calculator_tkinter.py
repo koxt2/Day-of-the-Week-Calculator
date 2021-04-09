2062370d72275ff2ac8bfb98b8798d244cdee08c
Desktop <devavictrix@protonmail.com>
2021-04-09 15:56:50 +0100

    commit 3

--- Day of the Week Calculator/Day of week calculator_tkinter.py
+++ Day of the Week Calculator/Day of week calculator_tkinter.py
@@ -1,9 +1,11 @@
 # Day of the week calculator
-
 # Using Zeller's congruence...
 
 from tkinter import *
 from tkinter.ttk import *
+
+
+
 
 #Create root window and a frame in the window
 root = Tk()
@@ -11,6 +13,9 @@ root.title("Day of the Week Calculator")
 root.geometry("800x300")
 frame = Frame(root)
 frame.grid()
+
+
+
 
 #Create labels
 lbl_question = Label(frame, text="Please enter the date in the boxes below...")
@@ -25,6 +30,9 @@ lbl_month.grid(row=1, column=1)
 lbl_year = Label(frame, text="Year", width=10)
 lbl_year.grid(row=1, column=2)
 
+
+
+#Get date (day,month,year)
 #Get day
 def selected_day(event):
     global q
@@ -35,7 +43,6 @@ combo_day['values'] = list (range(1,32))
 combo_day.set("Choose a day")
 combo_day.grid(row=2, column=0)
 combo_day.bind("<<ComboboxSelected>>", selected_day)
-
 #Get month and determine month code
 month_code={"January":13,
             "February":14,
@@ -59,7 +66,6 @@ combo_month.grid(row=2, column=1)
 combo_month['values'] = list (month_code.keys())
 combo_month.set("Choose a month")
 combo_month.bind("<<ComboboxSelected>>", selected_month)
-
 #Get year
 def selected_year(event):
     global year_input
@@ -69,6 +75,24 @@ entry_year = Entry(frame)
 entry_year.grid(row=2, column=2)
 entry_year.insert(0,"Choose a year") 
 entry_year.bind("<KeyRelease>", selected_year) 
+
+
+
+#Interpret inputs
+#Leap year?
+def leap_year():
+    global leap
+    if (year_input%4) == 0:
+        if (year_input%100) == 0:
+            if (year_input%400) == 0:
+                leap = True
+            else: leap = False
+        else:
+            leap = True
+    else:
+        leap = False
+
+
 
 #Calibrate inputs
 def calibrate_inputs():
@@ -81,34 +105,39 @@ def calibrate_inputs():
         year=int(year_input)
     k = int(year%100)
     j = int(year//100)
-    lbl_calibrated = Label(frame, text=year).grid(row=4, column=3)
+    
+
 
 #Print inputs
 def print_day():
-    lbl_day = Label(frame, text=q).grid(row=4, column=0)
+    lbl_day = Label(frame, text=q).grid(row=3, column=0)
 
 def print_month():
-    lbl_month = Label(frame, text=m).grid(row=4, column=1)
+    lbl_month = Label(frame, text=m).grid(row=3, column=1)
 
 def print_year():
-    lbl_year = Label(frame, text=year_input).grid(row=4, column=2)
+    lbl_year = Label(frame, text=year_input).grid(row=3, column=2)
 
-but_day = Button(frame, text="Show day", command=print_day).grid(row=3, column=0)
-but_month = Button(frame, text="Show month", command=print_month).grid(row=3, column=1)
-but_year = Button(frame, text="Show year", command=print_year).grid(row=3, column=2)
-but_calibrated = Button(frame, text="Show calibrated year", command=calibrate_inputs).grid(row=3, column=3)
+def print_leap():
+    if leap == True:
+        lbl_leap = Label(frame, text="Leap year!").grid(row=3, column=3)
+    else:
+        lbl_not_leap = Label(frame, text="Not a Leap!").grid(row=3, column=3)
+
+def print_calibrated_year():
+    lbl_calibrated = Label(frame, text=year).grid(row=3, column=4)
+    lbl_calibrated = Label(frame, text=j).grid(row=4, column=4)
+    lbl_calibrated = Label(frame, text=k).grid(row=5, column=4)
+
+but_process_data = Button(frame, text="Process data", command=lambda:[calibrate_inputs(), leap_year()]).grid(row=4, column=0)
+but_print_data = Button(frame, text="How inputted data", command=lambda:[print_day(), print_month(), print_year(), print_leap(), print_calibrated_year()]).grid(row=5, column=0)
+
+#Test data
+
 
 #Validity of date
     #Leap year?
-#if (year_input%4) == 0:
- #   if (year_input%100) == 0:
-  #      if (year_input%400) == 0:
-   #         year_leap = True
-    #    else: year_leap = False
-    #else:
-     #   year_leap = True
-#else:
- #   year_leap = False
+
 
     #Does the date lie within the missing dates of the Julian-Gregorian transition?
 #def jul_greg_missing_date():
