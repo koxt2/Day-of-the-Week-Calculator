32ae82706c931bada1d5fe0b8f4106023f0ee43f
Desktop <devavictrix@protonmail.com>
2021-04-10 00:17:48 +0100

    commit 10

--- Day of the Week Calculator/Day of week calculator_tkinter.py
+++ Day of the Week Calculator/Day of week calculator_tkinter.py
@@ -1,5 +1,5 @@
 # Day of the week calculator
-# Using Zeller's congruence...
+# Using Zeller's congruence
 
 from tkinter import *
 from tkinter.ttk import *
@@ -11,77 +11,44 @@ root.geometry("800x300")
 frame = Frame(root)
 frame.grid()
 
-##### Create labels #####
-lbl_day = Label(frame, text="Day")
-lbl_day.grid(row=1, column=0, padx=20)
+##### Dictionaries #####
+day_key={   0:"Saturday",
+            1:"Sunday",
+            2:"Monday",
+            3:"Tuesday",
+            4:"Wednesday",
+            5:"Thursday",
+            6:"Friday"}
 
-lbl_month = Label(frame, text="Month")
-lbl_month.grid(row=1, column=1, padx=20)
-
-lbl_year = Label(frame, text="Year")
-lbl_year.grid(row=1, column=2, padx=20)
+month_value={   "January":13,
+                "February":14,
+                "March":3,
+                "April":4,
+                "May":5,
+                "June":6,
+                "July":7,
+                "August":8,
+                "September":9,
+                "October":10,
+                "November:":11,
+                "December":12}
 
 ##### Get date (day,month,year) #####
-#Get day
 def selected_day(event):
     global q
     q = int(combo_day.get())
 
-combo_day = Combobox(frame)
-combo_day['values'] = list (range(1,32))
-combo_day.set("Choose a day")
-combo_day.grid(row=2, column=0)
-combo_day.bind("<<ComboboxSelected>>", selected_day)
-
-#Get month and determine month value
-month_value={"January":13,
-            "February":14,
-            "March":3,
-            "April":4,
-            "May":5,
-            "June":6,
-            "July":7,
-            "August":8,
-            "September":9,
-            "October":10,
-            "November:":11,
-            "December":12}
 def selected_month(event):
-    global m
+    global m, month
     m = int(month_value[combo_month.get()])
-        
-combo_month = Combobox(frame)
-combo_month.grid(row=2, column=1)
-combo_month['values'] = list (month_value.keys())
-combo_month.set("Choose a month")
-combo_month.bind("<<ComboboxSelected>>", selected_month)
 
-#Get year
 def selected_year(event):
-    global year_input
+    global year, year_input, k, j, leap
     year_input = int(entry_year.get())
-   
-entry_year = Entry(frame)
-entry_year.grid(row=2, column=2)
-entry_year.insert(0,"Choose a year") 
-entry_year.bind("<KeyRelease>", selected_year) 
-
-##### Process inputs #####
-#Extract usable data from year_input
-def calibrate_year():
-    global year
-    global k
-    global j 
     if m in [13,14]:
         year=int(year_input-1)
     else:
         year=int(year_input)
-    k = int(year%100)
-    j = int(year//100)
-
-#Leap year?
-def leap_year():
-    global leap
     if (year_input%4) == 0:
         if (year_input%100) == 0:
             if (year_input%400) == 0:
@@ -90,18 +57,12 @@ def leap_year():
         else:
             leap = True
     else:
-        leap = False
+        leap = False    
+    k = int(year%100)
+    j = int(year//100)
 
-##### Final calculation #####
-def valid_date():
-    day_key={   0:"Saturday",
-                    1:"Sunday",
-                    2:"Monday",
-                    3:"Tuesday",
-                    4:"Wednesday",
-                    5:"Thursday",
-                    6:"Friday"}
-    global invalid_date_jul_greg
+##### Check input is valid and perform calculation #####
+def calculation():
     if m in (4,6,9,11) and q not in range(1,31):
         output_text=("This date is invalid. You can't have", q, "days in that month")
         invalid_date = Label(frame, text=output_text).grid(row=3, column=2)
@@ -122,8 +83,35 @@ def valid_date():
             day = day_key[(q + 13*(m+1)//5 + k + k//4 + j//4 - 2*j) %7]
             print_result = Label(frame, text=day).grid(row=4, column=0)
 
+##### GUI #####
+lbl_day = Label(frame, text="Day")
+lbl_day.grid(row=1, column=0, padx=20)
 
-but_process_data = Button(frame, text="Process data", command=lambda:[calibrate_year(), leap_year(), valid_date()]).grid(row=2, column=3, padx=20)
+lbl_month = Label(frame, text="Month")
+lbl_month.grid(row=1, column=1, padx=20)
+
+lbl_year = Label(frame, text="Year")
+lbl_year.grid(row=1, column=2, padx=20)
+
+combo_day = Combobox(frame)
+combo_day['values'] = list (range(1,32))
+combo_day.set("Choose a day")
+combo_day.grid(row=2, column=0)
+combo_day.bind("<<ComboboxSelected>>", selected_day)
+
+combo_month = Combobox(frame)
+combo_month.grid(row=2, column=1)
+combo_month['values'] = list (month_value.keys())
+combo_month.set("Choose a month")
+combo_month.bind("<<ComboboxSelected>>", selected_month)
+
+entry_year = Entry(frame)
+entry_year.grid(row=2, column=2)
+entry_year.insert(0,"Choose a year") 
+entry_year.bind("<KeyRelease>", selected_year) 
+
+##### Provide result #####
+but_process_data = Button(frame, text="Process data", command=lambda:[calculation()]).grid(row=2, column=3, padx=20)
 
 root.mainloop()
 
